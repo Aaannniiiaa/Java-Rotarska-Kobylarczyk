2eed9ad2ca354c70fefbad2a0c5d893ffb7dd619
koby1a <kobylarczykmateusz8@gmail.com>
2026-03-23 17:44:40 +0100

    Dodano ProjectRepository oraz konfiguracje JPA

--- ProjectManagerApp/src/main/java/org/example/projectmanagerapp/ProjectManagerAppApplication.java
+++ ProjectManagerApp/src/main/java/org/example/projectmanagerapp/ProjectManagerAppApplication.java
@@ -2,8 +2,11 @@ package org.example.projectmanagerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
+@EntityScan(basePackages = "org.example.projectmanagerapp")
+@EnableJpaRepositories(basePackages = "org.example.projectmanagerapp")
 public class ProjectManagerAppApplication {
 
     public static void main(String[] args) {
---
+++ ProjectManagerApp/src/main/java/org/example/projectmanagerapp/ProjectRepository.java
@@ -0,0 +1,8 @@
+package org.example.projectmanagerapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
